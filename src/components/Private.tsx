de1d3b8674fca3325b66a1bf31bb21170464795e
Dmitry Lazarkov <dmitry.lazarkov@spbpu.com>
2024-03-12 22:53:41 +0300

    nod3-task1

---
+++ src/components/Private.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+import { Navigate } from "react-router-dom"
+import User from "../types/user"
+
+export interface PrivateProps {
+    user: User | null,
+    children: JSX.Element | JSX.Element[]
+}
+
+const Private: FC<PrivateProps> = ({user, children}) => {
+
+    if(user == null) {
+        return <Navigate to="/login" />
+    }
+    return children
+
+}
+
+export default Private
---
+++ src/types/user.ts
@@ -0,0 +1,5 @@
+export default interface User {
+    id: string, // if id is uuid or int (will be typed correctly later)
+    username: string
+    //TODO: add fields if needed later
+}
